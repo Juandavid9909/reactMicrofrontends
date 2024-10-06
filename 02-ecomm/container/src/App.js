afe2546e5b14b8a8d72b0b05faa6b1f6bd10aa55
Juan David <juandavid918@gmail.com>
2024-10-06 12:53:45 -0500

    Mostrando contenido del container y de marketing

--- 02-ecomm/container/src/App.js
+++ 02-ecomm/container/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return (
-        <h1>Hi there!</h1>
+        <div>
+            <h1>Hi there!</h1>
+
+            <hr />
+
+            <MarketingApp />
+        </div>
     );
 }
